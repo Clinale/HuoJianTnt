caf6fe3d50f8203c584489f3a871672d841b6306
clinale <clinale@foxmail.com>
2024-04-08 00:17:55 +0800

    修改为邮箱消息通知

--- main.py
+++ main.py
@@ -3,31 +3,37 @@ import sys
 import json
 import login 
 import getWebStatu
-import WeCom_nodream
+import yagmail
+
+def send_email(sender, passwd, receiver, contents):
+    subject = "火箭TNT签到"
+    # 初始化yagmail客户端
+    yag = yagmail.SMTP(user=sender, password=passwd, host='smtp.qq.com', encoding='GBK')
+    # 发送邮件
+    yag.send(to=receiver, subject=subject, contents=contents)
+    # 不需要手动关闭连接，yagmail会自动处理
+
 baseUrl = getWebStatu.getWebUrl()
 checkUrl = baseUrl + 'user/checkin'
 username = os.environ['USERNAME']
 password = os.environ['PASSWORD']
-# 初始化企业微信参数
-CORPID = os.environ['CORPID'] 
-CORPSECRET = os.environ['CORPSECRET']
-# 应用的ID
+# 初始化邮箱参数
+sender = os.environ['SENDER']
+sender_passwd = os.environ['SENDER_PASSWORD']
+receiver = os.environ['RECEIVER']
+
 if __name__ == "__main__":
-  loginCode =  login.login(username, password)
-  if loginCode != 1: 
-    print('登录失败')
-  session = login.session
-  # 请求签到
-  checkin = session.post(checkUrl).text
-  checkin = json.loads(checkin)
-  # 签到状态
-  if checkin['ret'] == 1:
-    AGENTID1 = os.environ['AGENTID1'] 
-    print(AGENTID1, CORPID, CORPSECRET,"1")
-    wecom = WeCom_nodream.WeCom(AGENTID1, CORPID, CORPSECRET)
-    wecom.sendCardMsg("火箭TNT签到成功",str(checkin['msg']),'https://github.com/1802024110/HuoJianTnt','火箭TNT')
-  else:
-    AGENTID2 = os.environ['AGENTID2'] 
-    print(AGENTID2, CORPID, CORPSECRET,"-1")
-    wecom = WeCom_nodream.WeCom(AGENTID2, CORPID, CORPSECRET)
-    wecom.sendCardMsg("火箭TNT签到失败",str(checkin['msg']),'https://github.com/1802024110/HuoJianTnt','火箭TNT')
+    loginCode =  login.login(username, password)
+    if loginCode != 1: 
+        print('登录失败')
+    session = login.session
+    # 请求签到
+    checkin = session.post(checkUrl).text
+    checkin = json.loads(checkin)
+    # 签到状态
+    if checkin['ret'] == 1:
+        contents = ["火箭TNT签到成功", checkUrl, str(checkin['msg']),'https://github.com/1802024110/HuoJianTnt','火箭TNT']
+    else:
+        contents = ["火箭TNT签到失败", checkUrl, str(checkin['msg']),'https://github.com/1802024110/HuoJianTnt','火箭TNT']
+    print(contents)
+    send_email(sender, sender_passwd, receiver, '\n'.join(contents))
